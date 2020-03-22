bb752e29d6f0e1128b4fa0f55a727a1842ca204f
numerx <i@justpic.org>
2020-03-22 23:01:42 +0800

    transfer

---
+++ tools/precache-manifest.cfcf45bf67e30a71a957bdda0b3c72cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac9359f710acf2f4d24e58ffcdbfd2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "713dfc55f5e9bcb7a041",
+    "url": "./static/css/main.f86bc672.chunk.css"
+  },
+  {
+    "revision": "55248d7afebedce34161",
+    "url": "./static/js/2.744c36fb.chunk.js"
+  },
+  {
+    "revision": "713dfc55f5e9bcb7a041",
+    "url": "./static/js/main.d77813cb.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
